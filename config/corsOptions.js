005f2f8192b1adf37aa6aee8796552ca592b499f
sebanputhen <sebastian.puthenveedu@gmail.com>
2025-03-21 14:17:05 +0530

    march 21new

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,18 +1,24 @@
 const allowedOrigins = require("./allowedOrigins");
 
-const corsOptions = 
-{
-    origin: (origin , callback)=>{
-        if(allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null , true)
+const corsOptions = {
+    origin: (origin, callback) => {
+        // If no origin is sent (like server-to-server requests), allow
+        if (!origin) {
+            callback(null, true);
+            return;
         }
-        else
-        {
-            callback(new Error("Not Allowed By CORS"))
+
+        // Check if the origin is in the allowed list
+        if (allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not Allowed By CORS"));
         }
     },
-    credentials : true ,
-    optionsSuccessStatus : 200 , 
-}
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Specify allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+    credentials: true, // Allow credentials (cookies, authorization headers)
+    optionsSuccessStatus: 200 
+};
 
-module.exports = corsOptions
+module.exports = corsOptions;
