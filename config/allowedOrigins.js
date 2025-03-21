b8ac6fe7c1d249fd705f4e2bcd52e05fdc11323e
sebanputhen <sebastian.puthenveedu@gmail.com>
2025-03-21 15:35:37 +0530

    march22m

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,16 +1,16 @@
 const allowedOrigins = [
-  "http://localhost:8082",
-  "https://tithe-backend.onrender.com",
-  "https://jeevan-1.onrender.com",  
-  "https://titheajce.netlify.app",
   "http://localhost:5000",
   "http://localhost:3000",
-  "http://localhost:3001", 
-  "https://jeevan-backend-n9hy.onrender.com",
-  "https://jeevan-bay.vercel.app",
-  "https://jeevan-frontend-two.vercel.app",
   "https://kalosavam-backend.vercel.app",
   "https://kalosavam.vercel.app",
 ];
 
 module.exports = allowedOrigins;
+// "http://localhost:3001", 
+// "https://jeevan-backend-n9hy.onrender.com",
+// "https://jeevan-bay.vercel.app",
+// "https://jeevan-frontend-two.vercel.app",
+// "http://localhost:8082",
+// "https://tithe-backend.onrender.com",
+// "https://jeevan-1.onrender.com",  
+// "https://titheajce.netlify.app",
