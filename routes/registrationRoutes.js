59503a9ddc45b7b231cb57d6b9efbfd6b4630789
sebanputhen <sebastian.puthenveedu@gmail.com>
2026-08-06 20:08:45 +0530

    aug1 700

--- routes/registrationRoutes.js
+++ routes/registrationRoutes.js
@@ -6,6 +6,7 @@ const registrationController = require('../controllers/registrationController');
 // Regular routes
 router.get('/', registrationController.getAllRegistrations);
 router.get('/:id', registrationController.getRegistration);
+router.post('/batch', registrationController.batchCreateRegistrations);
 router.post('/', registrationController.createRegistration);
 router.put('/:id', registrationController.updateRegistration);
 router.patch('/:id', registrationController.updateRegistration);
